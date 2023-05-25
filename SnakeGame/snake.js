39290ce6aec526637146c879efd855f3761677cf
Rafael Parra <700175134@fvtc.edu>
2023-05-24 23:54:58 -0500

    Updated Snake Game

--- SnakeGame/snake.js
+++ SnakeGame/snake.js
@@ -1,3 +1,6 @@
+"USE STRICT"
+
+let header = document.getElementById("header");
 
 //board
 var blocksize = 25;
@@ -6,11 +9,10 @@ var cols = 20;
 var board;
 var ctx;
 
-
-
 //snake head
 var snakeX = blocksize * 5;
 var snakeY = blocksize * 5;
+console.log(snakeX);
 var velocityX = 0;
 var velocityY = 0;
 
@@ -20,27 +22,35 @@ var snakeBody = [];
 var foodX;
 var foodY;
 
+//score
+let score = 0;
 var gameOver = false;
+let hs = document.getElementById('highscore');
+//local storage score
+let highscoreSesh = localStorage.getItem('highscore');
 
+
+
+//game load
 window.onload = function() {
  board = document.getElementById("board");
  board.height = rows * blocksize;
  board.width = cols * blocksize;
  ctx = board.getContext("2d"); //used for drawing on board
- 
+
+ //generate food
  placeFood();
  document.addEventListener("keyup", changeDirection);
 
- //update
+ //update canvas
  setInterval(update, 1000/10); //100 milliseconds
 
- //restart
+ //restart button
  let button = document.getElementById("button");
  button.onclick=function(){
-  location.reload();
+ location.reload();
  }
 }
-
 
 
 function update(){
@@ -55,12 +65,21 @@ if(gameOver){
  //food
  ctx.fillStyle="red";
  ctx.fillRect(foodX, foodY, blocksize, blocksize);
+ hs.textContent = highscoreSesh;
 
  //handles eating of food, creates new green square
  if(snakeX == foodX && snakeY == foodY){
   snakeBody.push([foodX, foodY]);
+  score++;
   placeFood();
+  playFoodSound();
 
+  //if saved score less than current score, set saved score to current
+  if(highscoreSesh < score){
+    localStorage.setItem('highscore', score);
+    hs.textContent = score;
+  }
+  
  }
 
  //attach head to body
@@ -71,7 +90,6 @@ if(gameOver){
   snakeBody[0] = [snakeX, snakeY];
  }
 
-
  //snake
  ctx.fillStyle="lime";
  snakeX += velocityX * blocksize;
@@ -81,20 +99,35 @@ if(gameOver){
   ctx.fillRect(snakeBody[i][0], snakeBody[i][1], blocksize, blocksize);
  }
 
- //game over conditions
+ //game over conditions 
+    //out of bounds
  if(snakeX < 0 || snakeX > cols*blocksize-1 || snakeY < 0 || snakeY > rows*blocksize-1){
   gameOver = true;
-  alert("Game Over");
+  playGameOver();
+  header.textContent = "Game Over";
+  header.style.color = "red";
+  score = 0;
+  hs.textContent = highscoreSesh;
  } 
+ //hits itself
  for(let i = 0; i<snakeBody.length; i++){
   if(snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
    gameOver = true;
-   alert("Game Over");
+   playGameOver();
+   header.textContent = "Game Over";
+   header.style.color = "red"; 
+   score = 0;
+   hs.textContent = highscoreSesh;
   }
  }
-
+ //show score
+ let sc = document.getElementById('score');
+ sc.textContent = score;
+ hs.textContent = localStorage.getItem('highscore');
 }
 
+
+//change directions
 function changeDirection(e){
     if(e.code == "ArrowUp" && velocityY != 1){
      velocityX = 0;
@@ -113,8 +146,21 @@ function changeDirection(e){
     }
 }
 
+//generate food
 function placeFood(){ 
  //0-1) * cols ->(0-19.9999) -> (0-19)   * 25
  foodX = Math.floor(Math.random() * cols) * blocksize;
  foodY = Math.floor(Math.random() * rows) * blocksize;
 }
+
+
+//sounds
+function playFoodSound(){
+    let sound = new Audio("audio/magic.wav");
+    sound.play();
+}
+function playGameOver(){
+    let gameOver = new Audio("audio/gameOver.wav");
+    gameOver.play();
+}
+
