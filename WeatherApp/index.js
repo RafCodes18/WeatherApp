a0ef0a27090d5219c0f0ae0577c144c1554b7a76
Rafael Parra <700175134@fvtc.edu>
2023-06-04 21:21:34 -0500

    Added WeatherAPP final updates,  projects page

--- WeatherApp/index.js
+++ WeatherApp/index.js
@@ -1,11 +1,11 @@
 const apiKEY = "eb5ffb1b54bc26f7055e2f4a9d32e586";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=germany";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=standard&q=germany";
 
 async function checkWeather(){
  const response = await fetch(apiURL + `&appid=${apiKEY}`);
  var data = await response.json();
 
- consoler.log(data);
+ console.log(data);
  
 }
 
