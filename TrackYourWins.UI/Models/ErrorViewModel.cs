2009ca87e9f93dd5d2d0b03e2d30bda2e013a846
Rafael Parra <rafael12@gmx.com>
2023-07-05 20:38:12 -0500

    added track your wins

---
+++ TrackYourWins.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrackYourWins.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
